3f4b3e8ab6e7fccf41814b89d2b6460c51aa6e8a
Jeremy Roberts <jaroberts@ksu.edu>
2016-10-31 07:44:25 -0500

    Added example where inline is meaningful and where c and c++ differ.

---
+++ eclipse/c++_vs_c_inline/function.hh
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+#include <cstdio>
+
+inline void function()
+{
+	printf("Hello World\n");
+}
+
+#endif /* FUNCTION_H_ */
---
+++ eclipse/c++_vs_c_inline/function2.hh
@@ -0,0 +1,6 @@
+#ifndef FUNCTION2_H_
+#define FUNCTION2_H_
+
+void function2();
+
+#endif /* FUNCTION2_H_ */
